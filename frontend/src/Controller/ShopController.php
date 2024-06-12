4e37bcc74ad448e644ad2bb2207674c36c42cd7b
Youssef-Meziani <mezianiyoussef564@gmail.com>
2024-06-12 12:38:02 +0100

    shop page setup

--- frontend/src/Controller/ShopController.php
+++ frontend/src/Controller/ShopController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ShopController extends AbstractController
 {
+    private ProductService $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
     #[Route('/shop', name: 'app_shop')]
     public function index(): Response
     {
+        $products = $this->productService->getAllProducts();
+
         return $this->render('shop.twig', [
-            'controller_name' => 'ShopController',
+            'products' => $products,
         ]);
     }
 }
