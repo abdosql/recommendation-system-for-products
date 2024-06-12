b2ce8b93a3d117361e744357b39bbbf7e17e5c8f
abdosql <abdosql@users.noreply.github.com>
2024-06-12 13:47:09 +0200

    model change

--- backend/app/cosine_similarity_model.py
+++ backend/app/cosine_similarity_model.py
@@ -5,47 +5,27 @@ import pickle
 
 class CosineSimilarityModel:
     def __init__(self):
-        self.user_similarity_matrix = None
-        self.item_similarity_matrix = None
+        self.model = None
         self.users = []
         self.items = []
 
     def train_model(self, user_item_matrix):
-        # Compute the cosine similarity matrix for users and items
-        self.user_similarity_matrix = cosine_similarity(user_item_matrix)
-        self.item_similarity_matrix = cosine_similarity(user_item_matrix.T)
+        similarity_matrix = cosine_similarity(user_item_matrix)
+        self.model = similarity_matrix
 
     def save_model(self, filepath):
         with open(filepath, 'wb') as f:
-            pickle.dump((self.user_similarity_matrix, self.item_similarity_matrix), f)
+            pickle.dump(self.model, f)
 
     def load_model(self, filepath):
         with open(filepath, 'rb') as f:
-            self.user_similarity_matrix, self.item_similarity_matrix = pickle.load(f)
+            self.model = pickle.load(f)
 
     def set_users_items(self, users, items):
         self.users = users
         self.items = items
 
-    def predict(self, user_index, item_index):
-        # Predict the rating by taking a weighted sum of the ratings given by similar users/items
-        user_similarity = self.user_similarity_matrix[user_index]
-        item_similarity = self.item_similarity_matrix[item_index]
-
-        user_ratings = self.user_item_matrix[user_index]
-        item_ratings = self.user_item_matrix[:, item_index]
-
-        user_based_prediction = np.dot(user_similarity, user_ratings) / np.sum(user_similarity)
-        item_based_prediction = np.dot(item_similarity, item_ratings) / np.sum(item_similarity)
-
-        # Combine the two predictions
-        return (user_based_prediction + item_based_prediction) / 2
-
     def recommend(self, user_index, num_recommendations=5):
-        # Recommend items based on the highest predicted ratings for the user
-        user_ratings = self.user_item_matrix[user_index]
-        predicted_ratings = np.dot(self.user_similarity_matrix[user_index], self.user_item_matrix) / np.sum(
-            self.user_similarity_matrix[user_index])
-
-        recommended_item_indices = np.argsort(predicted_ratings)[::-1][:num_recommendations]
+        user_similarities = self.model[user_index]
+        recommended_item_indices = np.argsort(user_similarities)[::-1][:num_recommendations]
         return recommended_item_indices
--- backend/app/interaction_weights.py
+++ backend/app/interaction_weights.py
@@ -1,18 +1,20 @@
 interaction_weights = {
-    'rating': 1.0,
-    'view': 0.1,
+    'onProductRated': 1.0,
+    'onProductViewed': 0.1,
     'add_to_cart': 0.5,
-    'purchase': 1.0,
-    'favorite': 0.7
+    'onProductCommented': 0.5,
+    'onCartItemAdded': 0.7,
+    'onCartItemRemoved': 0
 }
 
 def map_interactions_to_ratings(interactions):
     ratings = []
     for interaction in interactions:
         weight = interaction_weights.get(interaction['type'], 0)
+        rating_value = interaction.get('rating', 1) * weight
         ratings.append({
             'user_id': interaction['user_id'],
             'item_id': interaction['item_id'],
-            'rating': interaction.get('rating', 1) * weight
+            'rating': rating_value
         })
     return ratings
